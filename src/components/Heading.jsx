bca773532759604a7ee21238e3dca4ddedac3502
etemkapisiz <92946054+etemkapisiz@users.noreply.github.com>
2022-08-24 13:19:48 +0300

    Initial commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <h1 contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+  );
+}
+
+export default Heading;
